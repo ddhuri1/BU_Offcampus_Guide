739b9d7f27013387a2158bb3d7c9e880640676a6
Purva Thakkar <pthakka4@binghamton.edu>
2019-03-25 18:11:31 +0000

    Dead links, but main page set up

---
+++ config/routes.rb
@@ -0,0 +1,24 @@
+Rails.application.routes.draw do
+  #get 'users/new'
+  #get 'users/index'
+  get 'welcome/index'
+  #get 'welcome/register'
+  #resources :users
+  #resources :houses
+  #resources :grocery
+  #resources :travel
+  #root 'welcome#index'
+  #resources :users
+  #root :to => "welcome#index"
+  #root :new_user => "users#new"
+  #root :guest => "users#guest"
+  
+  Rails.application.routes.draw do
+  get 'welcome/index' => 'welcome#index'
+  get 'users/new_user' => 'users#new_user'
+  get 'users/guest' => 'users#guest'
+  get 'users/login' => 'users#login'
+  root 'welcome#index'
+end
+
+end
